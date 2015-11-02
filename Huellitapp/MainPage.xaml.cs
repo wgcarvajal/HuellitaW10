eab9915429a0a779b01fcd8418449e79e0a51ff3
wilson carvajal <wgcarvajal@unicauca.edu.co>
2015-11-02 17:02:04 -0500

    creacion del proyecto huellitaap

---
+++ Huellitapp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Parse;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Huellitapp
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Frame rootFrame;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+            rootFrame = Window.Current.Content as Frame;
+            hayLogin();
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (ParseUser.CurrentUser != null)
+            {
+                rootFrame.Navigate(typeof(MascotasPage));
+            }
+        }
+
+        public void hayLogin()
+        {
+
+        }
+
+        private async void iniciarSesion(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                await ParseUser.LogInAsync(user.Text, pass.Text);
+                rootFrame.Navigate(typeof(MascotasPage));
+
+            }
+            catch (Exception exception)
+            {
+
+            }
+
+
+        }
+    }
+}
